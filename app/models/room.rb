2f1b7fce6a05019dfc95a7639628c1888a1e443b
Apple <apple@ShinkenoMacBook.local>
2023-03-15 19:27:39 +0900

    “messageモデル作成＋その他のルーチング設定”

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,4 @@
 class Room < ApplicationRecord
   has_many :user_room
+  has_many :messages
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_03_15_100630) do
+ActiveRecord::Schema.define(version: 2023_03_15_101853) do
+
+  create_table "messages", force: :cascade do |t|
+    t.text "message"
+    t.integer "user_id", null: false
+    t.integer "room_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["room_id"], name: "index_messages_on_room_id"
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
 
   create_table "rooms", force: :cascade do |t|
     t.integer "user_id", null: false
@@ -41,6 +51,8 @@ ActiveRecord::Schema.define(version: 2023_03_15_100630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "messages", "rooms"
+  add_foreign_key "messages", "users"
   add_foreign_key "rooms", "users"
   add_foreign_key "user_rooms", "rooms"
   add_foreign_key "user_rooms", "users"
