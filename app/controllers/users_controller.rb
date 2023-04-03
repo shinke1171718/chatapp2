44c2ee232cf25870f9ea2c99caebec938d77068a
Apple <apple@ShinkenoMacBook.local>
2023-04-03 19:09:30 +0900

    binding.pryが反応しないため一度コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,16 @@
 class UsersController < ApplicationController
   def show
-    @user = User.find(params[:format])
-    @current_user_room = User_room.where(user_id: current_user.id)
+    def roomcreate
+      @user = User.find(params[:format])
+      @other = User.where.not(id: @user.id)
+      binding.pry
+      unless @user.id == @other.user.id
+        puts "aaaaa"
+        if @room.nil?
+          @room = Room.new()
+          @user_room = User_room.new(@other.user.id)
+        end
+      end
+    end
   end
 end
