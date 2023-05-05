20174d9a0db3339e2f32e2c473bcb247d3956cb7
Apple <apple@ShinkenoMacBook-3.local>
2023-05-05 15:23:22 +0900

    “roomモデルとuserモデルの間に中間テーブルを設置した。”

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,8 +1,12 @@
 class RoomsController < ApplicationController
   def create
-    @room = Room.find_by(current_user_id: current_user.id, user_id: params[:user_id]) || @room = Room.find_by(current_user_id: params[:user_id], user_id: current_user.id)
+    @user1 = User.find(params[:current_user_id])
+    @user2 = User.find(params[:user_id])
+    @room = Room.joins(:user_rooms).where(user_rooms: { user_id: [@user1.id, @user2.id] }).group(:id).having('count(*) = 2').first
     if @room.nil?
-      @room = Room.create(current_user_id: current_user.id, user_id: params[:user_id])
+      @room = Room.create(user_id: @user1.id)
+      UserRoom.create(user_id: params[:user_id], room_id: @room.id)
+      UserRoom.create(user_id: current_user.id, room_id: @room.id)
       flash[:now] = "新しくROOMを作成しました。"
       redirect_to "/rooms/#{@room.id}"
     else
